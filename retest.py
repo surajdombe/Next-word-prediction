32aa4829a02fb7377542525195833276669c8a57
Suraj Dombe <suraj25dombe@gmail.com>
2025-04-10 20:39:51 +0530

    Update retest.py

--- retest.py
+++ retest.py
@@ -1,13 +1,4 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Feb 22 18:24:21 2018
 
-@author: Apostolidou Maria
-
-Description: Test 
-             Loads Markov Chain from .json and tests method find_next_state
-"""
 from chain import MarkovChain
 
 new_chain = MarkovChain.from_json('markov_chain_1st_order.json')
